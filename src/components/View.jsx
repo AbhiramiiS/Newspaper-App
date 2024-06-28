163054626fbedf2317eff60d7aba0a590040980d
AbhiramiiS <abhiramis5002@gmail.com>
2024-06-28 10:07:25 +0530

    add newspaper

---
+++ src/components/View.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const View = () => {
+    return (
+        <div>
+            <NavBar />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">SI NO</th>
+                                            <th scope="col">Customer name</th>
+                                            <th scope="col">Mobile</th>
+                                            <th scope="col">Plan</th>
+                                            <th scope="col">Review</th>
+                        
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Chinchu</td>
+                                            <td>xxxxxxxx09</td>
+                                            <td>3 months</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Gopikrishna</td>
+                                            <td>xxxxxxxx98</td>
+                                            <td>1 year</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Adithya</td>
+                                            <td>xxxxx87xxx</td>
+                                            <td>3 months</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">4</th>
+                                            <td>Ananthakrishnan</td>
+                                            <td>xxxxxx87xx</td>
+                                            <td>1 year</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">5</th>
+                                            <td>Devipriya</td>
+                                            <td>xxxxxxx54x</td>
+                                            <td>1 year</td>
+                                            <td>5 STAR</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">6</th>
+                                            <td>Sahad</td>
+                                            <td>xxxx65xxxx</td>
+                                            <td>3 months</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">7</th>
+                                            <td>Parvathi</td>
+                                            <td>xxxxxxxx87</td>
+                                            <td>1 year</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">8</th>
+                                            <td>Ebin</td>
+                                            <td>xxxxxxxx98</td>
+                                            <td>3 months</td>
+                                            <td>5 STAR</td>
+                                            
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">9</th>
+                                            <td>Abhi</td>
+                                            <td>xxxxxxxx34</td>
+                                            <td>1 year</td>
+                                            <td>5 STAR</td>
+                                           
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">10</th>
+                                            <td>Anisha</td>
+                                            <td>xxxxxxx54x</td>
+                                            <td>3 months</td>
+                                            <td>5 STAR</td>
+                                           
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default View
